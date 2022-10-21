d171d01fe535886fe08e98214165ee47a246dc1a
Link <michael.sparks003@gmail.com>
2022-10-21 07:20:57 -0400

    Rewrites tests and reveals a flaw
    In order to fulfill the goal of full test coverage, this commit rewrites
    and adds tests for both the clock and time_receiver.
    
    The tests revealed a flaw in how tokio's bounded channels work here.
    Adding more than a channel can hold causes the next receiver to return
    an error. Once a receiver gets the error it clears all overflowed items
    from the channel and leaves an amount of items equal to the channel's
    size. The items left are the latest ones put in the channel.

--- tests/clock_tests.rs
+++ tests/clock_tests.rs
@@ -1,25 +1,57 @@
 use rust_clock::Clock;
 use std::thread;
 
-#[test]
-fn counting_works() {
-  let mut clock = Clock::custom(1).unwrap_or_else(|err| panic!("Couldn't create clock: '{err}'"));
-  let mut previous_time = 0;
+#[cfg(test)]
+mod clock {
+  use super::*;
 
-  clock.start();
-  clock.wait_for_tick(); // waits so the time will be 1
+  #[test]
+  fn counting_works() {
+    let mut clock =
+      Clock::custom(1) // 1ms tickrate to make the test go faster
+        .unwrap_or_else(|error| {
+          panic!("An error has occurred while creating the clock: '{error}'")
+        });
+    let mut previous_time = 0;
 
-  for _ in 0..2000 {
-    let time = clock.time();
+    let expected_final_time = 1001;
 
-    assert!(time == previous_time + 1);
+    clock.start();
+    clock.wait_for_tick(); // waits a tick so the time will be 1
 
-    previous_time = time;
+    for _ in 0..1000 {
+      let time = clock.time();
+
+      assert!(time == previous_time + 1);
+
+      previous_time = time;
+    }
+
+    let final_time = clock
+      .stop()
+      .unwrap_or_else(|error| panic!("An error has occurred while stopping the clock: '{error}'"));
+
+    assert_eq!(expected_final_time, final_time);
   }
 
-  clock
-    .stop()
-    .unwrap_or_else(|err| panic!("An error has occurred while stopping the clock: '{err}'"));
+  #[test]
+  fn wait_for_x_ticks_logic() {
+    let mut clock =
+      Clock::custom(1) // 1ms tickrate to make the test go faster
+        .unwrap_or_else(|error| {
+          panic!("An error has occurred while creating the clock: '{error}'")
+        });
+    let expected_final_time = 10;
+
+    clock.start();
+    clock.wait_for_x_ticks(10);
+
+    let final_time = clock
+      .stop()
+      .unwrap_or_else(|error| panic!("An error has occurred while stopping the clock: '{error}'"));
+
+    assert_eq!(expected_final_time, final_time);
+  }
 }
 
 #[cfg(test)]
@@ -27,45 +59,74 @@ mod time_receiver {
   use super::*;
 
   #[test]
-  fn logic_works() {
-    let mut clock = Clock::new().unwrap_or_else(|err| panic!("Couldn't create clock: '{err}'"));
+  fn time_receiver_works() {
+    let mut clock =
+      Clock::custom(1) // 1ms tickrate to make the test go faster
+        .unwrap_or_else(|error| {
+          panic!("An error has occurred while creating the clock: '{error}'")
+        });
 
-    clock.start();
-
+    let expected_final_time = 1001;
+    let mut previous_time = 0;
     let mut time_receiver = clock.spawn_receiver();
 
-    let handle = thread::spawn(move || {
-      for _ in 0..5 {
-        let _ = time_receiver.time();
+    clock.start();
+    clock.wait_for_tick();
+
+    let _ = thread::spawn(move || {
+      let mut previous_time = 0;
+
+      for _ in 0..1000 {
+        let time = time_receiver.time();
+
+        assert!(time == previous_time + 1);
+
+        previous_time = time;
       }
     });
 
-    for _ in 0..5 {
-      let _ = clock.time();
+    for _ in 0..1000 {
+      let time = clock.time();
+
+      assert!(time == previous_time + 1);
+
+      previous_time = time;
     }
 
-    handle
-      .join()
-      .unwrap_or_else(|err| panic!("An error has occurred while joining the handle: '{err:?}'"));
-
-    clock
+    let final_time = clock
       .stop()
-      .unwrap_or_else(|err| panic!("An error has occurred while stopping the clock: '{err}'"));
+      .unwrap_or_else(|error| panic!("An error has occurred while stopping the clock: '{error}'"));
+
+    assert_eq!(expected_final_time, final_time);
   }
 
   #[test]
-  fn creating_receiver_before_starting_clock() {
-    let mut clock = Clock::new().unwrap_or_else(|err| panic!("Couldn't create clock: '{err}'"));
-    let mut time_receiver = clock.spawn_receiver();
+  #[ignore]
+  // When adding to a broadcast channel that's full, you will get an error
+  // on the receiver's side
+  fn time_receiver_methods() {
+    let mut clock =
+      Clock::custom(1) // 1ms tickrate to make the test go faster
+        .unwrap_or_else(|error| {
+          panic!("An error has occurred while creating the clock: '{error}'")
+        });
 
     clock.start();
 
+    let mut time_receiver = clock.spawn_receiver();
+
+    time_receiver.time();
+    // time_receiver.wait_for_x_ticks(5);
+    // time_receiver.wait_for_time(10);
     let time = time_receiver.time();
 
-    let last_time = clock
-      .stop()
-      .unwrap_or_else(|err| panic!("An error has occurred while stopping the clock: '{err}'"));
+    // clock.time();
+    // let time = clock.time();
 
-    assert_eq!(last_time, time);
+    let final_time = clock
+      .stop()
+      .unwrap_or_else(|error| panic!("An error has occurred while stopping the clock: '{error}'"));
+
+    assert_eq!(time + 1, final_time);
   }
 }
